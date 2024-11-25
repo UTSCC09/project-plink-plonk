185ae1f72ac51cc5a9e8e0a52d16c5e8d6b20139
m2zhang <marilyn2zhang@gmail.com>
2024-11-24 21:37:47 -0500

    feat: length

--- frontend/src/routes/Lobby.jsx
+++ frontend/src/routes/Lobby.jsx
@@ -9,7 +9,7 @@ import Webcam from "../components/Webcam";
 
 import { generateProblemText, generateProblem } from "../js/problemBank.mjs";
 
-const RACE_LENGTH = 3; // PLACEHOLDER
+const RACE_LENGTH = 10; // PLACEHOLDER
 
 export async function loader({ params }) {
   const { lobbyId } = params;
